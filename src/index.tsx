48f980a2c01b1090399a492de8aebd997346cb09
Shiezza <shiezza@csa360software.com>
2024-10-14 01:04:32 +0800

    added tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -54,25 +54,30 @@ const App = () => {
   };
 
   return (
-    <div className='app'>
-      <h1>My To-Do List</h1>
-      <div className='input-section'>
+    <div className='max-w-[500px] mx-auto my-12 p-5 bg-gray-100 rounded-lg shadow-lg text-center font-sans'>
+      <h1 className='font-bold mb-5 text-3xl text-teal-700'>My To-Do List</h1>
+      <div className='flex justify-center mb-5'>
         <input
           type='text'
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder='Enter a new task'
-          className='input-field'
+          className='p-[10px] text-base rounded-[5px] border border-gray-300 w-[70%] mr-[10px] outline-none transition-colors duration-200 focus:border-blue-500'
         />
-        <button onClick={addToDo} className='add-button'>
+        <button
+          onClick={addToDo}
+          className='px-5 py-2.5 bg-[#81abda] text-white border-none rounded-[5px] cursor-pointer text-base transition-colors duration-200 hover:bg-[#0056b3]'
+        >
           Add
         </button>
       </div>
-      <ul className='todo-list'>
+      <ul className='list-none p-0 flex flex-col'>
         {todos.map((todo) => (
           <li
             key={todo.id}
-            className={`todo-item ${todo.done ? 'done' : ''}`}
+            className={`flex justify-between items-center pl-[50px] pr-[50px] pt-[10px] cursor-pointer ${
+              todo.done ? 'line-through text-gray-500' : ''
+            }`}
             onClick={() => toggleDone(todo.id)}
           >
             {todo.text}
@@ -82,7 +87,7 @@ const App = () => {
                 removeTodo(todo.id);
               }}
               icon={faTrashCan}
-              className='delete-icon'
+              className='ml-[10px] cursor-pointer'
             />
           </li>
         ))}
